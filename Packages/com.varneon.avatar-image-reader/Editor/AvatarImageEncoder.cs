f6851b0b763e37c0eec73cfc2d8a5fc2798512c7
Varneon <me@varneon.com>
2022-08-17 03:03:23 -0700

    Update AvatarImageEncoder.cs
    -Update to latest file from upstream UTF8 branch

--- Packages/com.varneon.avatar-image-reader/Editor/AvatarImageEncoder.cs
+++ Packages/com.varneon.avatar-image-reader/Editor/AvatarImageEncoder.cs
@@ -27,8 +27,6 @@
 // released under the terms of the License used by "https://github.com/Miner28/AvatarImageReader" at the time of writing,
 // included in the header of this C# file.
 
-#if UNITY_EDITOR && !COMPILER_UDONSHARP
-
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -59,7 +57,7 @@ namespace AvatarImageDecoder
             availableAvatars = availableAvatars.Append("avtr_ffffffff_ffff_ffff_ffff_ffffffffffff").ToArray();
             Debug.Log($"Starting Multi Avatar Image Encoder");
             Debug.Log($"Input character count: {input.Length}");
-            
+
             int imageByteCount = (width * height * 4) - headerSize;
             Debug.Log($"Image byte count: {imageByteCount}");
             int imageCharCount = imageByteCount / 2;
@@ -68,7 +66,7 @@ namespace AvatarImageDecoder
             if (outputImageCount == 0) outputImageCount = 1;
             Debug.Log($"Output Image count: {outputImageCount}");
 
-            if (outputImageCount - 1 <= availableAvatars.Length-1)
+            if (outputImageCount - 1 <= availableAvatars.Length - 1)
             {
                 Texture2D[] outTex = new Texture2D[outputImageCount];
                 string[] outputStrings = new string[outputImageCount];
@@ -79,13 +77,13 @@ namespace AvatarImageDecoder
                     int length = Mathf.Min(imageCharCount, input.Length - startIndex);
 
                     Texture2D inputTexture = new Texture2D(width, height, TextureFormat.RGBA32, false);
-                    
+
                     outputStrings[i] = input.Substring(startIndex, length);
-                    
+
                     string snippet = outputStrings[i].Length > 30 ? "..." + outputStrings[i].Substring(0, 30) + "..." : "string too short to get snippet";
-                    Debug.Log($"Encoding Image {i+1} / {outputImageCount}: Offset {startIndex}; Length {length}; Header Avatar: {availableAvatars[i+1]}; String snippet: {snippet}");
-                    
-                    outTex[i] = EncodeUTF16Text(outputStrings[i], availableAvatars[i+1], inputTexture);
+                    Debug.Log($"Encoding Image {i + 1} / {outputImageCount}: Offset {startIndex}; Length {length}; Header Avatar: {availableAvatars[i + 1]}; String snippet: {snippet}");
+
+                    outTex[i] = EncodeUTF16Text(outputStrings[i], availableAvatars[i + 1], inputTexture);
                 }
 
                 return outTex;
@@ -95,7 +93,7 @@ namespace AvatarImageDecoder
                 throw new Exception("Not enough avatar IDs were provided to encode the provided string");
             }
         }
-        
+
         /// <summary>
         /// This is a C# implementation of "https://github.com/Miner28/AvatarImageReader/tree/main/Assets/AvatarImageDecoder/Python%20Encoder/gen.py".
         /// The Python script implementation is authoritative over this C# implementation.
@@ -115,7 +113,7 @@ namespace AvatarImageDecoder
 
             if (!string.IsNullOrEmpty(avatar) && Regex.IsMatch(avatar, VRChatApiTools.avatar_regex))
             {
-                
+
                 avatar = avatar.Replace("avtr_", "").Replace("-", "");
                 byte[] hex = StringToByteArray(avatar);
                 foreach (byte b in hex.Reverse())
@@ -126,26 +124,26 @@ namespace AvatarImageDecoder
             }
             else
             {
-                foreach (var b in new byte[] {255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255})
+                foreach (byte b in new byte[] { 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255 })
                 {
                     textbyteArray = textbyteArray.Prepend<byte>(b).ToArray();
                 }
             }
-            
-            var lengthOfTextbyteListWith4Bytes = textbyteArray.Length;
-            var totalBytesWith4Bytes = BitConverter.GetBytes(lengthOfTextbyteListWith4Bytes).Reverse().ToArray();
+
+            int lengthOfTextbyteListWith4Bytes = textbyteArray.Length;
+            byte[] totalBytesWith4Bytes = BitConverter.GetBytes(lengthOfTextbyteListWith4Bytes).Reverse().ToArray();
 
             if (textbyteArray.Length % 4 != 0)
             {
-                textbyteArray = textbyteArray.Append((byte) 16).ToArray();
+                textbyteArray = textbyteArray.Append((byte)16).ToArray();
             }
             if (textbyteArray.Length % 4 != 0)
             {
-                textbyteArray = textbyteArray.Append((byte) 16).ToArray();
+                textbyteArray = textbyteArray.Append((byte)16).ToArray();
             }
             if (textbyteArray.Length % 4 != 0)
             {
-                textbyteArray = textbyteArray.Append((byte) 16).ToArray();
+                textbyteArray = textbyteArray.Append((byte)16).ToArray();
             }
 
             Texture2D img;
@@ -155,26 +153,26 @@ namespace AvatarImageDecoder
             }
             else
             {
-                var imageWidth = 128;
-                var imageHeight = 96;
-                
+                int imageWidth = 128;
+                int imageHeight = 96;
+
                 img = new Texture2D(imageWidth, imageHeight, TextureFormat.RGBA32, false);
             }
-            
-            var pixels = Enumerable.Repeat(new Color32(255, 255, 255 ,255), img.width * img.height).ToArray();
-            
+
+            Color32[] pixels = Enumerable.Repeat(new Color32(255, 255, 255, 255), img.width * img.height).ToArray();
+
             textbyteArray = textbyteArray.Prepend(totalBytesWith4Bytes[3]).ToArray();
             textbyteArray = textbyteArray.Prepend(totalBytesWith4Bytes[2]).ToArray();
             textbyteArray = textbyteArray.Prepend(totalBytesWith4Bytes[1]).ToArray();
             textbyteArray = textbyteArray.Prepend(totalBytesWith4Bytes[0]).ToArray();
 
 
-            
 
-            for (int i = 0; i < pixels.Length ; i++)
+
+            for (int i = 0; i < pixels.Length; i++)
             {
-                pixels[i] = new Color32(textbyteArray[i* 4], textbyteArray[i* 4 + 1], textbyteArray[i* 4 + 2], textbyteArray[i* 4 + 3]);
-                if (textbyteArray.Length < i* 4 +5)
+                pixels[i] = new Color32(textbyteArray[i * 4], textbyteArray[i * 4 + 1], textbyteArray[i * 4 + 2], textbyteArray[i * 4 + 3]);
+                if (textbyteArray.Length < i * 4 + 5)
                 {
                     break;
                 }
@@ -184,8 +182,104 @@ namespace AvatarImageDecoder
 
             return img;
         }
-        
-        public static byte[] StringToByteArray(string hex) {
+
+
+        /// <summary>
+        /// Encodes UTF-8 string input into Texture2D[]
+        /// </summary>
+        /// <param name="input"></param>
+        /// <param name="availableAvatars"></param>
+        /// <param name="width"></param>
+        /// <param name="height"></param>
+        /// <returns></returns>
+        public static Texture2D[] EncodeUTF8Text(string input, string[] availableAvatars, int width, int height)
+        {
+            availableAvatars = availableAvatars.Append("avtr_ffffffff_ffff_ffff_ffff_ffffffffffff").ToArray(); //End Avatar
+
+            var bytesToEncode = Encoding.UTF8.GetBytes(input);
+            int bytesToEncodeLength = bytesToEncode.Length;
+
+            var maxBytes = (width * height - 5) * 4;
+
+            List<byte[]> byteSegments = new List<byte[]>();
+
+            for (int i = 0; i < bytesToEncodeLength / maxBytes + 1; i++)
+            {
+                byteSegments.Add(bytesToEncode.Slice(i * maxBytes, maxBytes));
+            }
+
+
+            if (byteSegments.Count > availableAvatars.Length)
+            {
+                Debug.LogError($"More byteSegments than available avatars! This shouldn't be possible. ({byteSegments.Count} segments, {availableAvatars.Length} avatars)");
+                return null;
+            }
+
+            Texture2D[] outputTextures = new Texture2D[byteSegments.Count];
+
+            for (int i = 0; i < byteSegments.Count; i++)
+            {
+                Texture2D texture2D = new Texture2D(width, height, TextureFormat.RGBA32, false);
+
+                string nextAvatar = i == byteSegments.Count - 1 ? "avtr_ffffffff_ffff_ffff_ffff_ffffffffffff" : availableAvatars[i + 1];
+
+                EncodeUTF8Text(byteSegments[i], nextAvatar, ref texture2D);
+
+                outputTextures[i] = texture2D;
+            }
+
+            return outputTextures;
+
+        }
+
+        private static void EncodeUTF8Text(byte[] bytesToEncode, string avatar, ref Texture2D texture2D)
+        {
+            int lengthOfTextbyteListWith4Bytes = bytesToEncode.Length;
+            byte[] totalBytesWith4Bytes = BitConverter.GetBytes(lengthOfTextbyteListWith4Bytes).Reverse().ToArray();
+
+            if (!string.IsNullOrEmpty(avatar) && Regex.IsMatch(avatar, VRChatApiTools.avatar_regex))
+            {
+                avatar = avatar.Replace("avtr_", "").Replace("-", "");
+                byte[] hex = StringToByteArray(avatar);
+                foreach (byte b in hex.Reverse())
+                {
+                    bytesToEncode = bytesToEncode.Prepend<byte>(b).ToArray();
+                }
+            }
+            else
+            {
+                foreach (byte b in new byte[] { 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255 })
+                    bytesToEncode = bytesToEncode.Prepend<byte>(b).ToArray();
+            }
+
+
+            bytesToEncode = bytesToEncode.Prepend(totalBytesWith4Bytes[3]).ToArray();
+            bytesToEncode = bytesToEncode.Prepend(totalBytesWith4Bytes[2]).ToArray();
+            bytesToEncode = bytesToEncode.Prepend(totalBytesWith4Bytes[1]).ToArray();
+            bytesToEncode = bytesToEncode.Prepend(totalBytesWith4Bytes[0]).ToArray();
+
+            while (bytesToEncode.Length % 4 != 0)
+            {
+                bytesToEncode = bytesToEncode.Append((byte)16).ToArray();
+            }
+
+            Color32[] pixels = Enumerable.Repeat(new Color32(255, 255, 255, 255), texture2D.width * texture2D.height).ToArray();
+
+
+            for (int i = 0; i < pixels.Length; i++)
+            {
+                pixels[i] = new Color32(bytesToEncode[i * 4], bytesToEncode[i * 4 + 1], bytesToEncode[i * 4 + 2], bytesToEncode[i * 4 + 3]);
+                if (bytesToEncode.Length < i * 4 + 5)
+                {
+                    break;
+                }
+            }
+            texture2D.SetPixels32(pixels.Reverse().ToArray());
+        }
+
+
+        public static byte[] StringToByteArray(string hex)
+        {
             return Enumerable.Range(0, hex.Length)
                 .Where(x => x % 2 == 0)
                 .Select(x => Convert.ToByte(hex.Substring(x, 2), 16))
@@ -194,4 +288,16 @@ namespace AvatarImageDecoder
     }
 }
 
-#endif
+public static class Extensions
+{
+    public static T[] Slice<T>(this T[] source, int index, int length)
+    {
+        if (index + length > source.Length)
+        {
+            length = source.Length - index;
+        }
+        T[] slice = new T[length];
+        Array.Copy(source, index, slice, 0, length);
+        return slice;
+    }
+}
